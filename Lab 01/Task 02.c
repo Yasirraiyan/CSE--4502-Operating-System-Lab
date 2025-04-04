adb5b63631536841fccca50afb1627ba1ff6d6dd
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-04-04 19:27:39 +0600

    Create Task 02.c

---
+++ Lab 01/Task 02.c
@@ -0,0 +1,52 @@
+
+#include<lib/gcc.h>
+
+
+  static unsigned int NUM_PAGES;
+  
+  
+   struct ATStruct
+   {
+       
+       unsigned int perm;
+       
+       unsigned int allocated;
+   };
+   
+   
+     static struct  ATStruct AT[l<<20];
+     
+     
+       unsigned int get_nps(void)
+       {
+           
+           return NUM_PAGES;
+       }
+    void set_nps(unsigned int nps)
+    {
+        
+        NUM_PAGES=nps;
+    }
+    
+      unsigned int at_is_norm(unsigned int page_index)
+      {
+          
+          return AT[page_index]> 1 ? 1:0;
+      }
+      
+        void at_set_perm(unsigned int page_index,unsigned int perm)
+         {
+             AT[page_index].perm=perm;
+             AT[page_index].allocated=0;
+         }
+         
+     unsigned int at_is_allocated(unsigned int page_index)
+     {
+         
+         return AT[page_index],allocated ? 1:0;
+     }
+     
+     void at_set_allocated(unsigned int page_index,unsigned int_allocated)
+     {
+          AT[page_index].allocated=allocated;
+     }
